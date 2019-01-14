5f8cb2982b6df9849b3428e3249cc29dadae5c5e
sunweifx <sunweifx@feinno.com>
2019-01-14 16:26:07 +0800

    试用小程序插件
    快捷代码片段创建页面周期

--- pages/appCenter/appCenter.js
+++ pages/appCenter/appCenter.js
@@ -1,65 +1,38 @@
+//Page Object
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log('into appcenter')
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //options(Object)
+  onLoad: function(options){
     
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onReady: function(){
     
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  onShow: function(){
     
   },
+  onHide: function(){
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
   },
+  onUnload: function(){
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
   },
+  onPullDownRefresh: function(){
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
   },
+  onReachBottom: function(){
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  },
+  onShareAppMessage: function(){
+
+  },
+  onPageScroll: function(){
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
+
   }
-})
+});
